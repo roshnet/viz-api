a717f00323bf8abd3bcdd2998d33e8906125b29e
skamranahmed <skahmed@myamu.ac.in>
2020-02-07 16:33:01 +0530

    Modified the excel_to_JSON to return list of items from json of marks

--- excel_to_JSON.py
+++ excel_to_JSON.py
@@ -1,4 +1,15 @@
 from excel2json import convert_from_file
+import os,json
 
 EXCEL_FILE = 'marks.xlsx'
 convert_from_file(EXCEL_FILE)
+marks_in_json = EXCEL_FILE.split('.')[0] +'.json'
+os.rename('Sheet1.json', marks_in_json)
+
+def get_list_from_json(marks_in_json):
+    with open(marks_in_json) as json_file:
+        data = json.load(json_file)
+        return data
+
+list_from_json = get_list_from_json(marks_in_json)
+print(list_from_json)
