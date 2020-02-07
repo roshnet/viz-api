68fbb5947dac06a0d01b67bc5dcf06bdc6c09009
skamranahmed <skahmed@myamu.ac.in>
2020-02-07 19:19:19 +0530

    Added get_names in helpers.py

--- app/preprocessing/helpers.py
+++ app/preprocessing/helpers.py
@@ -16,3 +16,12 @@ def get_name_from_json(file_path):
 
 
     return 1
+
+names = []
+def get_names(marks_in_json):
+    global names
+    with open(marks_in_json) as json_file:
+        data = json.load(json_file)
+        for items in data:
+            names.append(items['Name'])
+        return names
