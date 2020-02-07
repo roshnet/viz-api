97bb9be362c9268138cdd35114726dd397b3f11f
Roshan Sharma <reprogram46@gmail.com>
2020-02-08 00:05:27 +0530

    chore: Fix some import shit

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,2 +1,3 @@
 import app.routes.auth.login
 import app.routes.auth.signup
+import app.routes.actions
---
+++ app/routes/actions/__init__.py
@@ -0,0 +1,4 @@
+import app.routes.actions.display
+import app.routes.actions.download_pdf
+import app.routes.actions.ping
+import app.routes.actions.upload
